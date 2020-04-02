b4d2b762dce3f0bc53f30dca4d85c9c8f73948c9
Ying <yfang829@gmail.com>
2020-04-02 15:52:44 +0200

    test error

--- TpCamille/src/test/java/fr/isika/andre/Apptest.java
+++ TpCamille/src/test/java/fr/isika/andre/Apptest.java
@@ -4,19 +4,16 @@ import org.junit.Test;
 public class Apptest {
 	WhoIam whoiam = new WhoIam();
 	@Test
-	//renvoie Bonjour je suis Camille
 	public final void testCamarche() {
 		assertEquals("je mappelle Camille", whoiam.whoAreYou("Camille"));
 		
 	}
 	@Test
-	//renvoie une erreur vide
 	public final void testErreurVide() {
 		assertEquals("Vous ntes pas connu de nos services", whoiam.whoAreYou(""));
 	}
 	@Test
-	//renvoie une erreur autre nom
 	public final void testErreurNom() {
-		assertEquals("Vous ntes pas connu de nos services", whoiam.whoAreYou("toto"));
+		assertEquals("Vous ntes pas connu de nos services", whoiam.whoAreYou("toto"))
 	}
 }
