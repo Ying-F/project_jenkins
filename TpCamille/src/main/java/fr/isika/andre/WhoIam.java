4727d34a0ef0010cfc137733f499bc00248b174b
Ying <yfang829@gmail.com>
2020-04-02 15:41:17 +0200

    test

--- TpCamille/src/main/java/fr/isika/andre/WhoIam.java
+++ TpCamille/src/main/java/fr/isika/andre/WhoIam.java
@@ -4,11 +4,11 @@ public class WhoIam {
 	
 	public static String whoAreYou(String name) {
 		
-		String reponse = "Vous n'êtes pas connu de nos services";
+		String reponse = "Vous ntes pas connu de nos services";
 		
 		if(name.equals("Camille")) {
 			
-			reponse = "je m'appelle "+ name;
+			reponse = "je mappelle "+ name;
 		}
 		
 		return reponse;
